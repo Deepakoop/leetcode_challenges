bc5b7718499233bddb44fb3ff01345a008668aee
Deepak kumar chhaunkar <deepakkumarchhaunkar1@gmail.com>
2026-07-09 21:39:02 +0530

    Time: 79 ms (100%), Space: 45.3 MB (45.45%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.py
@@ -1,17 +1,11 @@
-from typing import List
-
 class Solution:
-    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-        # g[i] will store the component ID for node i
+    def pathExistenceQueries(self, n, nums, maxDiff, queries):
         g = [0] * n
         cnt = 0
         
-        # Segment the sorted array into components
         for i in range(1, n):
-            # If the gap between adjacent elements is too large, it's a new component
             if nums[i] - nums[i - 1] > maxDiff:
                 cnt += 1
             g[i] = cnt
             
-        # For each query, they are connected if they belong to the same component
         return [g[u] == g[v] for u, v in queries]
