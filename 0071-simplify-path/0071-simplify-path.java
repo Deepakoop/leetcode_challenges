a2d3727ff30879c6c80b6aeb652bf0ad68357dd3
Deepak kumar chhaunkar <deepakkumarchhaunkar1@gmail.com>
2026-07-27 09:52:24 +0530

    Time: 5 ms (52.11%), Space: 44.5 MB (94.25%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        
+        String[] components = path.split("/");
+        
+        for (String dir : components) {
+            if (dir.isEmpty() || dir.equals(".")) {
+                continue;
+            }
+            if (dir.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pollLast();
+                }
+            } else {
+                stack.offerLast(dir);
+            }
+        }
+        
+        return "/" + String.join("/", stack);
+    }
+}
