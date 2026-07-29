9c8117aa496ce234da452765a8fae20dc807dfa9
Deepak kumar chhaunkar <deepakkumarchhaunkar1@gmail.com>
2026-07-29 13:18:39 +0530

    Time: 21 ms (24.12%), Space: 97.9 MB (65.75%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(n, k, 1, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    private void dfs(int n, int k, int start, List<Integer> path, List<List<Integer>> ans) {
+        if (path.size() == k) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i <= n; ++i) {
+            path.add(i);
+            dfs(n, k, i + 1, path, ans);
+            path.remove(path.size() - 1);
+        }
+    }
+}
