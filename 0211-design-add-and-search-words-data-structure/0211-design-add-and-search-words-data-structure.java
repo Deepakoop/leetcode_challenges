f52e8d09721962509f584f730c56fae7ab161743
Deepak kumar chhaunkar <deepakkumarchhaunkar1@gmail.com>
2026-08-03 09:59:55 +0530

    Time: 203 ms (53.98%), Space: 274.2 MB (57.95%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,54 @@
+class WordDictionary {
+
+    private static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEndOfWord = false;
+    }
+
+    private final TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+            }
+            curr = curr.children[idx];
+        }
+        curr.isEndOfWord = true;
+    }
+    
+    public boolean search(String word) {
+        return searchHelper(word, 0, root);
+    }
+
+    private boolean searchHelper(String word, int index, TrieNode node) {
+        if (index == word.length()) {
+            return node.isEndOfWord;
+        }
+
+        char c = word.charAt(index);
+
+        if (c == '.') {
+            for (int i = 0; i < 26; i++) {
+                if (node.children[i] != null) {
+                    if (searchHelper(word, index + 1, node.children[i])) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } else {
+            int idx = c - 'a';
+            if (node.children[idx] == null) {
+                return false;
+            }
+            return searchHelper(word, index + 1, node.children[idx]);
+        }
+    }
+}
